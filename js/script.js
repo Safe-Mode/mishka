605119744471e4b805d99965c4345613549f53fa
Safe_Mode <safe2mode@gmail.com>
2017-07-25 00:04:34 +0300

    Добавлены иконки контактов, справлены отступы

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ navToggle.addEventListener('click', function() {
 	'use strict';
 
 	var file     = 'img/sprite.svg',
-		revision = 12;
+		revision = 19;
 
 	if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
 		return true;
